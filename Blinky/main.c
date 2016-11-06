87dea0eadb4e576bbf7fde3d9936f67f5cd8d09a
DylanLamont <dylan1221@gmail.com>
2016-11-06 15:41:55 -0800

    timer set

--- Blinky/main.c
+++ Blinky/main.c
@@ -3,16 +3,25 @@
 /*
  * main.c
  */
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	P1DIR |= 0x01;
-	//P1OUT = 0x01;
-	int a = 0;
-for (;;){
-		P1OUT^=0x01;
-	for ( a = 0 ; a<200000; a++){
+	void main(void)
+	{
+	  WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
+	  P1DIR |= 0x01;                            // Set P1.0 to output direction
 
-	}
-}
-}
+	  for (;;)
+	  {
+	    volatile unsigned long i;
 
+	    P1OUT = 0x00;                          // Toggle P1.0 using exclusive-OR
+
+	    i = 3000000;                              // Delay
+	    do (i--);
+	    while (i != 0);
+
+	  volatile unsigned long x;
+	  P1OUT = 0x01;
+	    x = 125000;                              // Delay
+	    do (x--);
+	    while (x != 0);
+	  }
+}
